23a4e0f2464f1c01297e940eef51a896f5bd0cdc
senpai-kouhai <129678369+senpai-kouhai@users.noreply.github.com>
2023-04-13 09:35:53 +0000

    [Finish] 4-22

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,8 @@
 class UsersController < ApplicationController
+
   def show
     @user = User.find(params[:id])
-    @post_images = @user.post_images
+    @post_images = @user.post_images.page(params[:page])
   end
 
   def edit
@@ -20,4 +21,5 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).permit(:name, :profile_image)
   end
+
 end
